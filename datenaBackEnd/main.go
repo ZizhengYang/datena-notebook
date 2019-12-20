2fcc744ec15457bfc16831d1062fae5944d20ec9
zzyang <zzyang@iastate.com>
2019-12-20 15:13:43 -0600

    backend: initial commit

---
+++ datenaBackEnd/main.go
@@ -0,0 +1,15 @@
+package datenaBackEnd
+
+import (
+	_ "datenaBackEnd/datenaBackEnd/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
